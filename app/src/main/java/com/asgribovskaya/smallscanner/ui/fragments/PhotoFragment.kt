fe336c1c5416031d42eb94a28d9f2de4b9631971
chd-k <asgribovskaya@yandex.ru>
2023-06-05 18:48:28 +0300

    Update capture and save image

--- app/src/main/java/com/asgribovskaya/smallscanner/ui/fragments/PhotoFragment.kt
+++ app/src/main/java/com/asgribovskaya/smallscanner/ui/fragments/PhotoFragment.kt
@@ -1,15 +1,16 @@
 package com.asgribovskaya.smallscanner.ui.fragments
 
+import android.content.ContentValues
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.activity.result.contract.ActivityResultContracts.TakePicture
-import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -18,8 +19,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.asgribovskaya.smallscanner.databinding.FragmentPhotoBinding
 import com.asgribovskaya.smallscanner.ui.viewmodels.PhotoViewModel
 import kotlinx.coroutines.launch
-import java.io.File
-import java.text.DateFormat.getDateTimeInstance
+import java.text.DateFormat
 import java.util.Date
 
 
@@ -34,13 +34,12 @@ class PhotoFragment : Fragment() {
             viewModel.updateUri(uri!!)
         }
 
-    private val takePhoto = registerForActivityResult(TakePicture()) { photoTaken: Boolean ->
-        if (photoTaken && takenPhotoUri != null) {
-            viewModel.updateUri(takenPhotoUri!!)
+    private val takePhoto =
+        registerForActivityResult(TakePicture()) { photoTaken: Boolean ->
+            if (photoTaken) viewModel.updateUri(photoUri)
         }
-    }
 
-    private var takenPhotoUri: Uri? = null
+    private lateinit var photoUri: Uri
 
     private val viewModel: PhotoViewModel by viewModels()
 
@@ -70,21 +69,23 @@ class PhotoFragment : Fragment() {
                 pickPhoto.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
             }
             btnPhotoCamera.setOnClickListener {
-                takePhoto.launch(getUri())
-//                TODO: VM uri
+                takePhoto.launch(getUriOfAddedItem())
             }
         }
     }
 
-    private fun getUri(): Uri {
-        val photoName = "IMG_SMALLSCANNER_${getDateTimeInstance().format(Date())}.jpg"
-        val photoFile = File(requireContext().applicationContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES), photoName)
-        takenPhotoUri = FileProvider.getUriForFile(
-            requireContext(),
-            "com.asgribovskaya.smallscanner.fileprovider",
-            photoFile
-        )
-        return takenPhotoUri!!
+    private fun getUriOfAddedItem(): Uri {
+        val resolver = requireContext().applicationContext.contentResolver
+        val imagesCollection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+            } else MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val photoName = "img_SmallScanner_${DateFormat.getDateTimeInstance().format(Date())}.jpg"
+        val newPhoto = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, photoName)
+        }
+        photoUri = resolver.insert(imagesCollection, newPhoto)!!
+        return photoUri
     }
 
     private fun setUpCollectors() = viewLifecycleOwner.lifecycleScope.launch {
